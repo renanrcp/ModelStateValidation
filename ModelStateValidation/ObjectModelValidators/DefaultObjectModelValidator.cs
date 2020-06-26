5a83e76ae7ce807393a3e12b8a9258f12274a72b
renanrcp <renancamerapinheiro@gmail.com>
2020-06-26 18:45:07 -0300

    DefaultObjectValidator

---
+++ ModelStateValidation/ObjectModelValidators/DefaultObjectModelValidator.cs
@@ -0,0 +1,48 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ModelStateValidation
+{
+    internal class DefaultObjectValidator : ObjectModelValidator
+    {
+        private readonly MvcOptions _mvcOptions;
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="DefaultObjectValidator"/>.
+        /// </summary>
+        /// <param name="modelMetadataProvider">The <see cref="IModelMetadataProvider"/>.</param>
+        /// <param name="validatorProviders">The list of <see cref="IModelValidatorProvider"/>.</param>
+        /// <param name="mvcOptions">Accessor to <see cref="MvcOptions"/>.</param>
+        public DefaultObjectValidator(
+            IModelMetadataProvider modelMetadataProvider,
+            IList<IModelValidatorProvider> validatorProviders,
+            MvcOptions mvcOptions)
+            : base(modelMetadataProvider, validatorProviders)
+        {
+            _mvcOptions = mvcOptions;
+        }
+
+        public override ValidationVisitor GetValidationVisitor(
+            ActionContext actionContext,
+            IModelValidatorProvider validatorProvider,
+            ValidatorCache validatorCache,
+            IModelMetadataProvider metadataProvider,
+            ValidationStateDictionary validationState)
+        {
+            var visitor = new ValidationVisitor(
+                actionContext,
+                validatorProvider,
+                validatorCache,
+                metadataProvider,
+                validationState)
+            {
+                MaxValidationDepth = _mvcOptions.MaxValidationDepth,
+                ValidateComplexTypesIfChildValidationFails = _mvcOptions.ValidateComplexTypesIfChildValidationFails,
+            };
+
+            return visitor;
+        }
+    }
+}
