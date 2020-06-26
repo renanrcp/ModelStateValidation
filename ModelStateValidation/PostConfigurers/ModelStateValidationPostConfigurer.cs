5b02e303933426faef312fe7e7e43c57822a7b6e
renanrcp <renancamerapinheiro@gmail.com>
2020-06-26 18:48:22 -0300

    ModelStateValidationPostConfigurer

---
+++ ModelStateValidation/PostConfigurers/ModelStateValidationPostConfigurer.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Options;
+
+namespace ModelStateValidation
+{
+    internal class ModelStateValidationPostConfigurer : IConfigureOptions<MvcOptions>
+    {
+        private readonly IStringLocalizerFactory _stringLocalizerFactory;
+        private readonly IValidationAttributeAdapterProvider _validationAttributeAdapterProvider;
+        private readonly IOptions<MvcDataAnnotationsLocalizationOptions> _dataAnnotationLocalizationOptions;
+
+        public ModelStateValidationPostConfigurer(
+            IValidationAttributeAdapterProvider validationAttributeAdapterProvider,
+            IOptions<MvcDataAnnotationsLocalizationOptions> dataAnnotationLocalizationOptions)
+        {
+            if (validationAttributeAdapterProvider == null)
+            {
+                throw new ArgumentNullException(nameof(validationAttributeAdapterProvider));
+            }
+
+            if (dataAnnotationLocalizationOptions == null)
+            {
+                throw new ArgumentNullException(nameof(dataAnnotationLocalizationOptions));
+            }
+
+            _validationAttributeAdapterProvider = validationAttributeAdapterProvider;
+            _dataAnnotationLocalizationOptions = dataAnnotationLocalizationOptions;
+        }
+        public void Configure(MvcOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            options.ModelMetadataDetailsProviders.Add(new DefaultMetadataProvider(
+                options,
+                _dataAnnotationLocalizationOptions,
+                _stringLocalizerFactory));
+
+            options.ModelValidatorProviders.Add(new DefaultModelValidatorProvider(
+                _validationAttributeAdapterProvider,
+                _dataAnnotationLocalizationOptions,
+                _stringLocalizerFactory));
+        }
+    }
+}
