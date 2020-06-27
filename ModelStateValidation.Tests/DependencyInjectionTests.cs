7b95f16ebf44e9d17cc4295575c194b377f4eff8
renanrcp <renancamerapinheiro@gmail.com>
2020-06-26 22:33:07 -0300

    DependencyInjectionTests

--- ModelStateValidation.Tests/DependencyInjectionTests.cs
+++ ModelStateValidation.Tests/DependencyInjectionTests.cs
@@ -1,7 +1,11 @@
 using System;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Metadata;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Xunit;
 
@@ -13,6 +17,54 @@ namespace ModelStateValidation.Tests
         public void CanGetModelStateValidator()
         {
             var validator = _provider.GetService<IModelStateValidator>();
+
+            Assert.NotNull(validator);
+        }
+
+        [Fact]
+        public void CanGetOptions()
+        {
+            var options = _provider.GetService<IOptions<MvcOptions>>();
+
+            Assert.NotNull(options);
+        }
+
+        [Fact]
+        public void CanGetValidateAttributeAdapter()
+        {
+            var adapter = _provider.GetService<IValidationAttributeAdapterProvider>();
+
+            Assert.NotNull(adapter);
+        }
+
+        [Fact]
+        public void CanGetModelMetadataProvider()
+        {
+            var metadataProvider = _provider.GetService<IModelMetadataProvider>();
+
+            Assert.NotNull(metadataProvider);
+        }
+
+        [Fact]
+        public void CanGetLoggerFactory()
+        {
+            var loggerFactory = _provider.GetService<ILoggerFactory>();
+
+            Assert.NotNull(loggerFactory);
+        }
+
+        [Fact]
+        public void CanGetCompositeMetadataDetailsProvider()
+        {
+            var compositeMetadataDetailsProvider = _provider.GetService<ICompositeMetadataDetailsProvider>();
+
+            Assert.NotNull(compositeMetadataDetailsProvider);
+        }
+
+        [Fact]
+        public void CanGetObjectModelValidator()
+        {
+            var validator = _provider.GetService<IObjectModelValidator>();
 
             Assert.NotNull(validator);
         }
